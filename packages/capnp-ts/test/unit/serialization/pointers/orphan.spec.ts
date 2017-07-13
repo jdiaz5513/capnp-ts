91eae3ba0e537d130c3e044fca601c84124f5486
jdiaz5513 <jdiaz5513@gmail.com>
2017-07-12 20:21:55 -0400

    hit 100% coverage for capnp.Orphan (#12)
    Well, not quite 100% since Interface is still unimplemented, but close enough.

---
+++ packages/capnp-ts/test/unit/serialization/pointers/orphan.spec.ts
@@ -0,0 +1,175 @@
+import {Message, ObjectSize, Orphan, Struct} from '../../../../lib';
+import {Int32List} from '../../../../lib/serialization';
+import {tap} from '../../../util';
+
+  // LINT: This is how we nicely ask the compiler to let us access the internal properties for testing...
+  /* tslint:disable no-any no-unsafe-any */
+
+/** Just a silly struct that holds a single pointer to... itself? */
+
+class TestStruct extends Struct {
+
+  static readonly _displayName = 'TestStruct';
+  static readonly _id = 'f38ff832f33d57da';
+  static readonly _size = new ObjectSize(8, 2);
+
+  adoptTest(value: Orphan<TestStruct>): void {
+
+    this._getPointer(0).adopt(value);
+
+  }
+
+  disownTest(): Orphan<TestStruct> {
+
+    return this.getTest().disown();
+
+  }
+
+  getTest(): TestStruct {
+
+    return this._getPointerAs(0, TestStruct);
+
+  }
+
+  hasTest(): boolean {
+
+    return !this._getPointer(0)._isNull();
+
+  }
+
+  initTest(): TestStruct {
+
+    return this._initStructAt(0, TestStruct);
+
+  }
+
+  setTest(value: TestStruct): void {
+
+    this._getPointer(0)._copyFrom(value);
+
+  }
+
+  getFoo(): number {
+
+    return this._getUint32(0);
+
+  }
+
+  setFoo(value: number): void {
+
+    this._setUint32(0, value);
+
+  }
+
+  disownList(): Orphan<Int32List> {
+
+    return this.getList().disown();
+
+  }
+
+  getList(): Int32List {
+
+    return this._getPointerAs(1, Int32List);
+
+  }
+
+  initList(length: number): Int32List {
+
+    return this._initList(1, Int32List, length);
+
+  }
+
+}
+
+tap.test('new Orphan()', (t) => {
+
+  const message = new Message();
+  const root = message.initRoot(TestStruct);
+
+  const structOrphan = new Orphan(root) as any;
+
+  t.equal(structOrphan._size.dataByteLength, TestStruct._size.dataByteLength, 'should copy the data byte length');
+  t.equal(structOrphan._size.pointerLength, TestStruct._size.pointerLength, 'should copy the pointer count');
+
+  t.ok(root._isNull(), 'should zero out the struct pointer');
+
+  const list = new Message().initRoot(TestStruct).initList(2);
+
+  const listOrphan = new Orphan(list) as any;
+
+  t.equal(listOrphan._length, 2, 'should copy the list length');
+  t.equal(listOrphan._elementSize, Int32List._size, 'should copy the list element size');
+
+  t.ok(list._isNull(), 'should zero out the list pointer');
+
+  t.end();
+
+});
+
+tap.test('Orphan._moveTo()', (t) => {
+
+  const root = new Message().initRoot(TestStruct);
+  const oldChild = root.initTest();
+  const oldList = root.initList(2);
+  oldChild.setFoo(100);
+  oldList.set(1, 300);
+
+  const structOrphan = root.disownTest() as any;
+  const listOrphan = root.disownList() as any;
+
+  const newChild = root.initTest();
+  const newList = root.initList(5);
+
+  t.equal(newChild.getFoo(), 0, 'should not contain disowned struct data in new struct');
+  t.equal(newList.get(1), 0, 'should not contain disowned list data in new list');
+
+  newChild.setFoo(200);
+  newList.set(1, 400);
+
+  structOrphan._moveTo(newChild);
+  listOrphan._moveTo(newList);
+
+  t.equal(newChild.getFoo(), 100, 'should overwrite target struct pointer and keep old data');
+  t.equal(newList.get(1), 300, 'should overwrite target list pointer and keep old data');
+  t.equal(newList.getLength(), 2, 'should set the correct list length');
+
+  t.throws(() => {
+
+    structOrphan._moveTo(root.initTest());
+
+  }, undefined, 'should not allow re-adoption');
+
+  t.throws(() => {
+
+    const o = root.disownTest() as any;
+    o._moveTo(new Message().initRoot(TestStruct));
+
+  }, undefined, 'should not allow moving to a different message');
+
+  t.end();
+
+});
+
+tap.test('Orphan.dispose()', (t) => {
+
+  const root = new Message().initRoot(TestStruct);
+  root.initTest().setFoo(100);
+  root.initList(1).set(1, 200);
+
+  const structOrphan = root.disownTest();
+  const listOrphan = root.disownList();
+
+  t.equal(root.segment.getUint32(32), 100);
+  t.equal(root.segment.getInt32(60), 200);
+
+  structOrphan.dispose();
+
+  t.equal(root.segment.getUint32(32), 0);
+
+  listOrphan.dispose();
+
+  t.equal(root.segment.getInt32(60), 0);
+
+  t.end();
+
+});
