e6ff00ad53001db4e130dfe0e2867d06423759ab
jdiaz5513 <j@jdiaz.io>
2017-07-18 19:57:48 -0400

    be more careful about invalid list element types

--- packages/capnpc-ts/src/generators.ts
+++ packages/capnpc-ts/src/generators.ts
@@ -324,6 +324,11 @@ export function generateStructFieldMethods(
 
         listClass = `${fullClassName}._${properName}`;
 
+      } else if (listClass === void(0)) {
+
+        /* istanbul ignore next */
+        throw new Error(format(E.GEN_UNSUPPORTED_LIST_ELEMENT_TYPE, whichElementType));
+
       }
 
       const listClassIdentifier = ts.createIdentifier(listClass);
