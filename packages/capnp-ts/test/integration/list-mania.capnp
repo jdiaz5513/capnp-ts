901c4198da0bec452f6df34b55620d9f487df8f9
jdiaz5513 <j@jdiaz.io>
2017-07-18 19:44:32 -0400

    add some crazy list tests

---
+++ packages/capnp-ts/test/integration/list-mania.capnp
@@ -0,0 +1,59 @@
+# @author jdiaz5513
+
+using TS = import "../../src/std/ts.capnp";
+
+@0xb4dbefd56457c333;
+$TS.importPath("../../lib/index");
+
+struct ListMania {
+
+  # lol jk this is not supported
+  # anyPointerList @0 :List(AnyPointer);
+
+  boolList @0 :List(Bool);
+
+  # weeeeee a circular reference
+  compositeList @1 :List(ListManiaStruct);
+
+  dataList @2 :List(Data);
+
+  float32List @3 :List(Float32);
+
+  float64List @4 :List(Float64);
+
+  int8List @5 :List(Int8);
+
+  int16List @6 :List(Int16);
+
+  int32List @7 :List(Int32);
+
+  int64List @8 :List(Int64);
+
+  interfaceList @9 :List(ListManiaInterface);
+
+  textList @10 :List(Text);
+
+  uint8List @11 :List(UInt8);
+
+  uint16List @12 :List(UInt16);
+
+  uint32List @13 :List(UInt32);
+
+  uint64List @14 :List(UInt64);
+
+  voidList @15 :List(Void);
+
+}
+
+interface ListManiaInterface {
+
+  # weeee even more recursion
+  getListMania @0 () -> (result :List(ListManiaInterface));
+
+}
+
+struct ListManiaStruct {
+
+  void @0 :Void;
+
+}
---
+++ packages/capnp-ts/test/integration/list-mania.spec.ts
@@ -0,0 +1,87 @@
+/**
+ * A collection of tests for various list pointer behaviors, especially around handling of nested types.
+ *
+ * These tests are awesome.
+ *
+ * @author jdiaz5513
+ */
+
+import * as capnp from '../../lib';
+import {tap} from '../util';
+import {ListMania} from './list-mania.capnp';
+
+tap.test('loop de loop', (t) => {
+
+  // TODO: Interfaces are not implemented yet.
+
+  t.pass();
+
+  t.end();
+
+});
+
+tap.test('1 of each list', (t) => {
+
+  t.doesNotThrow(() => {
+
+    const m = new capnp.Message();
+    const listMania = m.initRoot(ListMania);
+
+    listMania.initBoolList(1);
+    listMania.initCompositeList(1);
+    listMania.initDataList(1);
+    listMania.initFloat32List(1);
+    listMania.initFloat64List(1);
+    listMania.initInt8List(1);
+    listMania.initInt16List(1);
+    listMania.initInt32List(1);
+    listMania.initInt64List(1);
+    listMania.initInterfaceList(1);
+    listMania.initTextList(1);
+    listMania.initUint8List(1);
+    listMania.initUint16List(1);
+    listMania.initUint32List(1);
+    listMania.initUint64List(1);
+    listMania.initVoidList(1);
+
+    const boolList = listMania.getBoolList();
+    const compositeList = listMania.getCompositeList();
+    const dataList = listMania.getDataList();
+    const float32List = listMania.getCompositeList();
+    const float64List = listMania.getFloat64List();
+    const int8List = listMania.getInt8List();
+    const int16List = listMania.getInt16List();
+    const int32List = listMania.getCompositeList();
+    const int64List = listMania.getCompositeList();
+    const interfaceList = listMania.getInterfaceList();
+    const textList = listMania.getTextList();
+    const uint8List = listMania.getUint8List();
+    const uint16List = listMania.getUint16List();
+    const uint32List = listMania.getUint32List();
+    const uint64List = listMania.getUint64List();
+    const voidList = listMania.getVoidList();
+
+    compositeList.set(0, compositeList.get(0));
+
+    boolList.disown().dispose();
+    compositeList.disown().dispose();
+    dataList.disown().dispose();
+    float32List.disown().dispose();
+    float64List.disown().dispose();
+    int8List.disown().dispose();
+    int16List.disown().dispose();
+    int32List.disown().dispose();
+    int64List.disown().dispose();
+    interfaceList.disown().dispose();
+    textList.disown().dispose();
+    uint8List.disown().dispose();
+    uint16List.disown().dispose();
+    uint32List.disown().dispose();
+    uint64List.disown().dispose();
+    voidList.disown().dispose();
+
+  });
+
+  t.end();
+
+});
