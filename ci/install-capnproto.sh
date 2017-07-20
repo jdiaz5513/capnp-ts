42e92380c2f4cd9c5bb5c077776bf66032d83c30
efokschaner <efokschaner@riotgames.com>
2017-07-20 00:45:51 -0700

    Fix install prefix

--- ci/install-capnproto.sh
+++ ci/install-capnproto.sh
@@ -7,6 +7,6 @@ set -exuo pipefail
 curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
 tar zxf capnproto-c++-0.6.1.tar.gz
 cd capnproto-c++-0.6.1
-./configure --prefix=$HOME/opt/bin
-make -j6 check
+./configure --prefix=$HOME/opt
+make -j6
 make install
