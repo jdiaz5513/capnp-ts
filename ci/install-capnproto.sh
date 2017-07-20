fcccb77bcfcb9c467f028e9cffe99dd01ed18a4f
efokschaner <efokschaner@riotgames.com>
2017-07-20 00:34:38 -0700

    Make install without sudo

--- ci/install-capnproto.sh
+++ ci/install-capnproto.sh
@@ -7,6 +7,6 @@ set -exuo pipefail
 curl -O https://capnproto.org/capnproto-c++-0.6.1.tar.gz
 tar zxf capnproto-c++-0.6.1.tar.gz
 cd capnproto-c++-0.6.1
-./configure
+./configure --prefix=$HOME/opt/bin
 make -j6 check
 make install
